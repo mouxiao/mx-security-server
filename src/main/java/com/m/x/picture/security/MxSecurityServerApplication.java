30186ebae12a0237591c0d61c47897830a4b90c7
mouxiao1014@gmail.com <muxiao911014>
2020-04-02 21:42:16 +0800

    init

---
+++ src/main/java/com/m/x/picture/security/MxSecurityServerApplication.java
@@ -0,0 +1,13 @@
+package com.m.x.picture.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxSecurityServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MxSecurityServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/m/x/picture/security/MxSecurityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m.x.picture.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxSecurityServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
